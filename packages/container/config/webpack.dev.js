27c3c5ab51c51120e19c3913ee45008c505830c2
Shankar Subramanyam <shankarblr@hotmail.com>
2021-04-18 12:04:47 -0500

    Created deployed scripts

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commmonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
@@ -19,9 +18,6 @@ const devServer = {
         marketing: 'marketing@http://localhost:8002/remoteEntry.js',
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 }
